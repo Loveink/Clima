7d724a6227ae4d364c97a028e89ee204cae28edb
Loveink <106765014+Loveink@users.noreply.github.com>
2023-05-01 17:13:46 +0300

    done view

--- ViewController.swift
+++ ViewController.swift
@@ -8,45 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     private lazy var backgroundView: UIImageView = {
         let view = UIImageView()
         view.image = UIImage(named: "background")
+        view.contentMode = .scaleAspectFill
+//        view.clipsToBounds = true
+        view.isUserInteractionEnabled = true
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
-    }()
-    
-    private lazy var mainStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.distribution = .fillEqually
-//        stackView.alignment = .center
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        return stackView
     }()
     
     private lazy var searchStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .horizontal
-        stackView.distribution = .fillEqually
-        //        stackView.alignment = .center
+        stackView.spacing = 10
+        stackView.distribution = .fillProportionally
+        stackView.isUserInteractionEnabled = true
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
     
     private lazy var locationButton: UIButton = {
         let button = UIButton()
-        button.backgroundColor = UIColor(named: "location.circle.fill")
+        let config = UIImage.SymbolConfiguration(
+            pointSize: 40, weight: .medium, scale: .default)
+        let image = UIImage(systemName: "location.circle.fill", withConfiguration: config)
+        button.setImage(image, for: .normal)
+        button.tintColor = .black
         button.translatesAutoresizingMaskIntoConstraints = false
-//        button.addTarget(self, action: #selector(locationPressed), for: .touchUpInside)
+        // button.addTarget(self, action: #selector(locationPressed), for: .touchUpInside)
         return button
     }()
     
     private lazy var searchButton: UIButton = {
         let button = UIButton()
-        button.backgroundColor = UIColor(named: "magnifyingglass")
+        let config = UIImage.SymbolConfiguration(
+            pointSize: 40, weight: .medium, scale: .default)
+        let image = UIImage(systemName: "magnifyingglass", withConfiguration: config)
+        button.setImage(image, for: .normal)
+        button.tintColor = .black
         button.translatesAutoresizingMaskIntoConstraints = false
-//        button.addTarget(self, action: #selector(searchPressed), for: .touchUpInside)
         return button
     }()
     
@@ -56,13 +58,17 @@ class ViewController: UIViewController {
         text.font = UIFont.systemFont(ofSize: 25, weight: .regular)
         text.textColor = .lightGray
         text.textAlignment = .right
+        text.backgroundColor = .placeholderText
+        text.layer.cornerRadius = 5
         text.translatesAutoresizingMaskIntoConstraints = false
         return text
     }()
     
     private lazy var conditionView: UIImageView = {
         let view = UIImageView()
-        view.image = UIImage(named: "sun.max")
+        view.image = UIImage(systemName: "sun.max")
+        view.tintColor = .black
+        view.contentMode = .scaleAspectFill
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
@@ -70,8 +76,6 @@ class ViewController: UIViewController {
     private lazy var temperatureStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .horizontal
-        stackView.distribution = .fillEqually
-        //        stackView.alignment = .center
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
@@ -80,20 +84,23 @@ class ViewController: UIViewController {
         let label = UILabel()
         label.text = "21"
         label.font = UIFont.systemFont(ofSize: 80, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     private lazy var temperatureLabel2: UILabel = {
         let label = UILabel()
         label.text = "°"
-        label.font = UIFont.systemFont(ofSize: 100, weight: .regular)
+        label.font = UIFont.systemFont(ofSize: 100, weight: .light)
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     private lazy var temperatureLabel3: UILabel = {
         let label = UILabel()
         label.text = "C"
-        label.font = UIFont.systemFont(ofSize: 100, weight: .regular)
+        label.font = UIFont.systemFont(ofSize: 100, weight: .light)
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
@@ -101,6 +108,8 @@ class ViewController: UIViewController {
         let label = UILabel()
         label.text = "Moscow"
         label.font = UIFont.systemFont(ofSize: 30, weight: .regular)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
@@ -112,13 +121,10 @@ class ViewController: UIViewController {
     
     private func subviews() {
         view.addSubview(backgroundView)
-        backgroundView.addSubview(mainStackView)
-        mainStackView.addSubview(searchStackView)
-        mainStackView.addSubview(conditionView)
-        mainStackView.addSubview(temperatureStackView)
-        mainStackView.addSubview(cityLabel)
-        
-        
+        backgroundView.addSubview(searchStackView)
+        backgroundView.addSubview(conditionView)
+        backgroundView.addSubview(temperatureStackView)
+        backgroundView.addSubview(cityLabel)
         searchStackView.addArrangedSubview(locationButton)
         searchStackView.addArrangedSubview(searchTextField)
         searchStackView.addArrangedSubview(searchButton)
@@ -128,61 +134,42 @@ class ViewController: UIViewController {
     }
     
     func setupConstraints() {
-//        NSLayoutConstraint.activate([
+        NSLayoutConstraint.activate([
             
-//            calculateView.topAnchor.constraint(equalTo: billTotalTextField.bottomAnchor, constant: 40),
-//            calculateView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-//            calculateView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-//            calculateView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-//
-//            billTotalLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-//            billTotalLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-//            billTotalLabel.heightAnchor.constraint(equalToConstant: 30),
-//
-//            billTotalTextField.topAnchor.constraint(equalTo: billTotalLabel.bottomAnchor, constant: 10),
-//            billTotalTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-//            billTotalTextField.heightAnchor.constraint(equalToConstant: 48),
-//
-//            selectTipLabel.topAnchor.constraint(equalTo: calculateView.topAnchor, constant: 20),
-//            selectTipLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
-//            selectTipLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
-//            selectTipLabel.heightAnchor.constraint(equalToConstant: 30),
-//
-//            percentButtonsStackView.topAnchor.constraint(equalTo: selectTipLabel.bottomAnchor, constant: 20),
-//            percentButtonsStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
-//            percentButtonsStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
-//            percentButtonsStackView.heightAnchor.constraint(equalToConstant: 30),
-//
-//            chooseSplitLabel.topAnchor.constraint(equalTo: percentButtonsStackView.bottomAnchor, constant: 30),
-//            chooseSplitLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
-//            chooseSplitLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
-//            chooseSplitLabel.heightAnchor.constraint(equalToConstant: 30),
-//
-//            splitStackView.topAnchor.constraint(equalTo: chooseSplitLabel.bottomAnchor, constant: 20),
-//            //            splitStackView.widthAnchor.constraint(equalToConstant: 214),
-//            splitStackView.bottomAnchor.constraint(equalTo: calculateButton.topAnchor, constant: 30),
-//            splitStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-//
-//            splitNumberLabel.widthAnchor.constraint(equalToConstant: 94),
-//            splitNumberLabel.heightAnchor.constraint(equalToConstant: 29),
-//            splitNumberLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -30),
-//
-//            chooseSplitStepper.widthAnchor.constraint(equalToConstant: 94),
-//            chooseSplitStepper.heightAnchor.constraint(equalToConstant: 29),
-//            chooseSplitStepper.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 30),
-//
-//            zeroButton.widthAnchor.constraint(greaterThanOrEqualToConstant: 60),
-//            zeroButton.heightAnchor.constraint(equalToConstant: 54),
-//            tenButton.widthAnchor.constraint(greaterThanOrEqualToConstant: 60),
-//            tenButton.heightAnchor.constraint(equalToConstant: 54),
-//            twentyButton.widthAnchor.constraint(greaterThanOrEqualToConstant: 60),
-//            twentyButton.heightAnchor.constraint(equalToConstant: 54),
-//
-//            calculateButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-//            calculateButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-//            calculateButton.heightAnchor.constraint(equalToConstant: 54)
-//        ])
-    }
-    
-}
+            backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            searchStackView.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor, constant: 20),
+            searchStackView.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor, constant: -20),
+            searchStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchStackView.bottomAnchor.constraint(equalTo: searchButton.bottomAnchor),
 
+            locationButton.leadingAnchor.constraint(equalTo: searchStackView.leadingAnchor),
+            locationButton.topAnchor.constraint(equalTo: searchStackView.topAnchor),
+            locationButton.heightAnchor.constraint(equalToConstant: 40),
+            locationButton.widthAnchor.constraint(equalToConstant: 40),
+            
+            searchTextField.leadingAnchor.constraint(equalTo: locationButton.trailingAnchor, constant: 10),
+            
+            searchButton.leadingAnchor.constraint(equalTo: searchTextField.trailingAnchor, constant: 10),
+            searchButton.trailingAnchor.constraint(equalTo: searchStackView.trailingAnchor),
+            searchButton.topAnchor.constraint(equalTo: searchStackView.topAnchor),
+            searchButton.heightAnchor.constraint(equalToConstant: 40),
+            searchButton.widthAnchor.constraint(equalToConstant: 40),
+            
+            conditionView.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor, constant: -20),
+            conditionView.topAnchor.constraint(equalTo: searchStackView.bottomAnchor, constant: 10),
+            conditionView.heightAnchor.constraint(equalToConstant: 120),
+            conditionView.widthAnchor.constraint(equalToConstant: 120),
+            
+            temperatureStackView.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor, constant: -20),
+            temperatureStackView.topAnchor.constraint(equalTo: conditionView.bottomAnchor, constant: 10),
+            temperatureLabel1.centerYAnchor.constraint(equalTo: temperatureLabel3.centerYAnchor),
+            
+            cityLabel.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor, constant: -20),
+            cityLabel.topAnchor.constraint(equalTo: temperatureStackView.bottomAnchor, constant: 10)
+        ])
+    }
+}
